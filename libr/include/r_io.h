6e110ef8f4c439622ae157447d660a06dba9f178
condret <condr3t@protonmail.com>
2020-12-16 22:08:00 +0100

    Fix build

--- libr/include/r_io.h
+++ libr/include/r_io.h
@@ -380,8 +380,8 @@ R_API RIOPlugin *r_io_plugin_get_default(RIO *io, const char *filename, bool man
 
 /* bank */
 R_API void r_io_map_bank(RIO *io, RIOBank *bank);
-R_API RIOBank* r_io_new_bank(RIO *io, const char *name);
-R_API bool r_io_bank_add_map(RIOBank *bank, RIOMap *map);
+R_API RIOBank* r_io_new_bank(const char *name);
+R_API bool r_io_bank_add_map(RIO *io, RIOBank *bank, ut32 map_id);
 R_API void r_io_bank_free(RIOBank *bank);
 R_API void r_io_bank_rename(RIOBank *bank, const char *name);
 
